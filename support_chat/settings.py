6a212d82aa35a1dd23e7a63e10f9b739514e32a3
Ryan Haas <haasrr@outlook.com>
2025-07-27 18:17:07 -0400

    update settings

--- support_chat/settings.py
+++ support_chat/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = getenv("SUPPORT_CHAT_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = getenv("SUPPORT_CHAT_ALLOWED_HOSTS").split(',')
 
